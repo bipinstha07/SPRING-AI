7c566c3318b626b5a958842a269bd80f4cc19d1a
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-09-29 00:03:13 -0500

    default options AI

--- src/main/java/com/spring/ai/config/ChatController.java
+++ src/main/java/com/spring/ai/config/ChatController.java
@@ -2,6 +2,7 @@ package com.spring.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +11,12 @@ public class ChatController {
 
     @Bean(name="openAIChatClient")
     public ChatClient openAi(OpenAiChatModel chatModel){
-        return ChatClient.builder(chatModel).build();
+        return ChatClient.builder(chatModel)
+                .defaultAdvisors()
+                .defaultSystem("You are God")
+                .defaultOptions(OpenAiChatOptions.builder().model("gpt-4o-mini").temperature(0.2).build())
+
+                .build();
     }
 
 
